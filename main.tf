8ea54e04228014ab62d8b374abfafdf92956c4af
Apth <17009502+AlmightyMango@users.noreply.github.com>
2023-03-24 03:25:32 -0600

    fix

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
   name = var.environment.name
-  cidr = 
+  cidr = "${var.environment.network_prefix}.0.0/16"
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
   enable_nat_gateway = true
