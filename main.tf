78d44334e7eb691bf7e4fc9dcab40bde9d3a9cec
Apth <17009502+AlmightyMango@users.noreply.github.com>
2023-03-24 03:22:15 -0600

    Refactor to use variables

--- main.tf
+++ main.tf
@@ -1,25 +1,26 @@
 data "aws_ami" "app_ami" {
   most_recent = true
+
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["var.ami_filter.name"]
   }
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-  owners = ["979382823631"] # Bitnami
+  owners = ["var.ami_filter.owner"] # Bitnami
 }
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
   enable_nat_gateway = true
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -32,9 +33,9 @@ module "autoscaling" {
   source   = "terraform-aws-modules/autoscaling/aws"
   version  = "6.9.0"
 
-  name     = "blog"
-  min_size = 1 
-  max_size = 2
+  name     = "${var.environment.name}-blog"
+  min_size = var.asg_min_size 
+  max_size = var.asg_max_size
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.alb.target_group_arns
@@ -54,7 +55,7 @@ module "alb" {
   security_groups    = module.blog_sg.security_group_id
   target_groups = [
     {
-      name_prefix      = "blog-"
+      name_prefix      = "${var.environment.name}-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -68,13 +69,13 @@ module "alb" {
     }
   ]
   tags = {
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
-  name    = "blog"
+  name    = "${var.environment.name}-blog"
   vpc_id              = module.blog_vpc.public_subnets[0]
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
