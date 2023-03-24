e35dd6ceb3c3b1e63a97a0718872f6c4d193dce3
Apth <17009502+AlmightyMango@users.noreply.github.com>
2023-03-24 01:43:23 -0600

    vpc + alb

--- main.tf
+++ main.tf
@@ -37,7 +37,11 @@ resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow HTTP and HTTPS in. Allow everything out"
 
-  vpc_id      = data.aws_vpc.default.id
+  vpc_zone_identifier = module.vpc.public_subnets
+  target_group_arns  = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  image_id           = data.aws_ami.app_ami.id
+  instance_type      = var.instance_type
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
@@ -47,7 +51,38 @@ resource "aws_security_group_rule" "blog_http_in" {
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.blog.id
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = module.blog_sg.security_group_id
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      target = {
+        target_id = aws_instance.blog.id
+        port = 80
+      }
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port        = 80
+      protocol    = "HTTP"
+      target_group_index = 0 
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
 }
 
 resource "aws_security_group_rule" "blog_https_in" {
